466d194e9968217801f178c514dd37bc73463f55
Graeme MacGilchrist <graemem@princeton.edu>
2020-12-07 23:15:37 -0500

    checking problemss with iap data - b-factor

--- so_decadal_variability/__init__.py
+++ so_decadal_variability/__init__.py
@@ -1,4 +1,4 @@
-import process
-import calc
-import process
-import wmt
+import so_decadal_variability.process as process
+import so_decadal_variability.calc as calc
+import so_decadal_variability.grid as grid
+import so_decadal_variability.wmt as wmt
